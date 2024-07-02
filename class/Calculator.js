ac4be5fd9e07eaf78a9769cb416f75545a65f993
CTZDev <carlosteranzavaleta@hotmail.com>
2024-07-01 19:43:02 -0500

    Update class Calculator (operations and transform values)

--- class/Calculator.js
+++ class/Calculator.js
@@ -1,15 +1,10 @@
 class Calculator {
-  firstValue = null;
-  secondValue = null;
-  op = null;
+  display = null;
 
-  constructor(firstValue, secondValue, op) {
-    this.firstValue = firstValue;
-    this.secondValue = secondValue;
-    this.op = op;
+  constructor(display) {
+    this.display = display;
   }
 
-  /* Arreglar */
   sum(a, b) {
     return a + b;
   }
@@ -23,7 +18,123 @@ class Calculator {
   }
 
   division(a, b) {
-    if (b === 0) return "You can't divide for zero";
+    if (b === 0) return 'Error matemático';
     return a / b;
   }
+
+  operation(a, b, sign) {
+    if (sign === '+') return this.sum(a, b);
+    if (sign === '-') return this.substract(a, b);
+    if (sign === 'x') return this.product(a, b);
+    if (sign === '/') return this.division(a, b);
+  }
+
+  operationFirstOrder() {
+    const firstOrder = ['x', '/'];
+    const size = this.display.length;
+    const regexNumbers = /^[0-9]$/;
+    let result = '';
+    let groupInit = '';
+    let groupEnd = '';
+    let sign = '';
+
+    if (this.display[0] === '-') groupInit += '-';
+    for (let i = groupInit.length; i < size; i++) {
+      let j = i;
+
+      while (j < size && regexNumbers.test(this.display[j])) {
+        groupInit += this.display[j];
+        j++;
+      }
+
+      sign = this.display[j];
+      let ans = '';
+      let hasGroupEnd = false;
+      /* Significa que es signo de primer orden */
+      while (firstOrder.includes(sign)) {
+        while (j < size && regexNumbers.test(this.display[++j])) {
+          groupEnd += this.display[j];
+        }
+
+        const prev = parseFloat(groupInit);
+        const last = parseFloat(groupEnd);
+        ans = this.operation(prev, last, sign);
+        sign = this.display[j];
+        groupInit = ans;
+        groupEnd = '';
+        hasGroupEnd = true;
+      }
+
+      if (hasGroupEnd) {
+        i = j - 1;
+        groupInit = '';
+        groupEnd = '';
+        result += ans;
+        hasGroupEnd = false;
+        continue;
+      }
+
+      if (groupInit === '') {
+        result += sign;
+      } else {
+        result += groupInit;
+        groupInit = '';
+        i = j - 1;
+      }
+    }
+
+    return result;
+  }
+
+  /* Aqui operar de izquierda a derecha (+/-) */
+  operationSecondOrder(op) {
+    let groupInit = '';
+    let groupEnd = '';
+    const size = op.length;
+    const regexNumbers = /^[0-9]$/;
+    let sign = '';
+    let isActiveGroups = false;
+
+    const firstValue = op[0];
+    if (firstValue === '-') groupInit += firstValue;
+
+    /* 13500-10+40 */
+    /* -13500-10+40 */
+    for (let i = groupInit.length; i < size; i++) {
+      let j = i;
+      while (j < size && regexNumbers.test(op[j])) {
+        groupInit += op[j];
+        j++;
+        isActiveGroups = true;
+      }
+
+      sign = op[j];
+      while (j < size && regexNumbers.test(op[++j])) {
+        groupEnd += op[j];
+        isActiveGroups = true;
+      }
+
+      const prev = parseFloat(groupInit);
+      const last = parseFloat(groupEnd);
+      let ans = this.operation(prev, last, sign);
+      groupInit = ans;
+      groupEnd = '';
+
+      if (isActiveGroups) {
+        isActiveGroups = false;
+        i = j - 1;
+      }
+    }
+
+    return groupInit;
+  }
+
+  calculator() {
+    const resultFirstOrder = this.operationFirstOrder();
+    const finalResult = this.operationSecondOrder(resultFirstOrder);
+    return finalResult;
+  }
 }
+
+const calc = new Calculator('-45x10x30-10+20/10x20');
+calc.calculator();
